2d344f607d9bcfda946f219ca070abcc155e44f9
dwinurseto <dwi.nurseto.com>
2024-10-28 10:29:51 +0700

    oct 2024

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
 require('@snipkode/server');
 // console.log('ini global object', global);
 
- ROUTE('GET /', function(){
+ ROUTE('GET /landingpage', function(){
      // var self = this;
      this.view('pages/landingpage');
  });
 
-ROUTE('GET /', function(){
-    // var self = this;
-    this.view('pages/todo');
-});
+ ROUTE('GET /todo', function(){
+     // var self = this;
+     this.view('pages/todo');
+ });
 
  ROUTE('GET /', function(){
      // var self = this;
      this.view('pages/dashboard');
  });
+ 
+ ROUTE('GET /buku', function(){
+    // var self = this;
+    this.view('pages/books');
+});
 
 HTTP('debug', {port:5000});
