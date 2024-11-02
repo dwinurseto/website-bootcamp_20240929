b3301f2859f8afb061e54f12246b7eb3b3377a4f
dwinurseto <dwi.nurseto.com>
2024-11-02 19:16:08 +0700

    improved layout admin agar reusable

--- index.js
+++ index.js
@@ -21,4 +21,14 @@ require('@snipkode/server');
     this.view('pages/books');
 });
 
+ROUTE('GET /penerbit', function(){
+    // var self = this;
+    this.view('pages/publisher');
+});
+
+ROUTE('GET /pengguna', function(){
+    // var self = this;
+    this.view('pages/users');
+});
+
 HTTP('debug', {port:5000});
